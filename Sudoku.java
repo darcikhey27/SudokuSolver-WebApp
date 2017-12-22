905a640578db51b5892879a6f0c41b7528795ea8
Darci Khey Saucedo <darcikhey27@users.noreply.github.com>
2017-12-21 16:12:12 -0800

    Working on candidates function

--- Sudoku.java
+++ Sudoku.java
@@ -1,67 +1,120 @@
-package sudoku;
-
-import java.util.Stack;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Sudoku {
-	private int[][] board;
-	private int size;
-	
-	public Sudoku(int[][] board) {
-		this.board = board;
-		
-	}
-	
-	public boolean isFull(int board[][]) {
-		for(int i = 0; i < board.length; i++) {
-			for(int j = 0; j < board[0].length; j++) {
-				if(board[i][j] == 0) {
-					return false;
-				}
-			}
-		}
-		return true;
-	}
-	public Stack<Integer> candidates(int[][] board, int i, int j) {
-		Stack<Integer> candidates = new Stack<Integer>();
-		
-		
-		return null;
-	}
-	public void print() {
-		for(int i =0; i < this.board.length; i++) {
-			for(int j = 0; j < board[0].length; j++) {
-				System.out.print(this.board[i][j]);
-			}
-			System.out.println();
-		}
-	}
-	
-	public static void main(String[] args) {
-		int b[][] = new int[4][4];
-		b[0][0] = 1;
-		b[0][1] = 0;
-		b[0][2] = 3;
-		b[0][3] = 0;
-		
-		b[1][0] = 1;
-		b[1][1] = 0;
-		b[1][2] = 3;
-		b[1][3] = 0;
-		
-		b[2][0] = 1;
-		b[2][1] = 0;
-		b[2][2] = 3;
-		b[2][3] = 0;
-		
-		b[3][0] = 1;
-		b[3][1] = 0;
-		b[3][2] = 3;
-		b[3][3] = 0;
-		
-		Sudoku sudoku = new Sudoku(b);
 
-		sudoku.print();
-		
-	}
+    private static class Cell {
+        int value;
+        int y;
+        int x;
+        List<Integer> candidates;
 
+        public Cell(int value, int x, int y) {
+            this.value = value;
+            this.x = x;
+            this.y = y;
+            this.candidates = new LinkedList<>();
+        }
+        @Override
+        public String toString() {
+            return this.value+" ("+this.x+","+this.y+")";
+        }
+    }
+
+    public void solve(Cell[][] grid) {
+        int i = 0;
+        int j = 0;
+        boolean foundVacant = false;
+
+        for(int x = 0; x < 4; x++) {
+            for(int y = 0; y < 4; y++) {
+                if(grid[x][y].value == 0) {
+                    i = x;
+                    j = y;
+                    foundVacant = true;
+                    break;
+                }
+            }
+            if(foundVacant) {
+                break;
+            }
+        }
+        List<Integer> candidates = getCandidates(grid, i, j);
+
+
+    }
+
+    private List<Integer> getCandidates(Cell[][] grid, int i, int j) {
+
+        List<Integer> rowValues = new LinkedList<>();
+        for(int y = 0; y < 4; y++) {
+            if(grid[i][y].value != 0) {
+                rowValues.add(grid[i][y].value);
+            }
+        }
+        List<Integer> colValues = new LinkedList<>();
+        for(int x = 0; x < 4; x++) {
+            if(grid[x][j].value != 0) {
+                colValues.add(grid[x][j].value);
+            }
+        }
+
+        rowValues.addAll(colValues);
+
+        int k = 0;
+        int l = 0;
+        if(i >= 0 && i <= 2) {
+            k = 0;
+        }
+        else if(i >= 3 && i <=5) {
+            k = 3;
+        }
+        else {
+            k = 6;
+        }
+
+        if(l >= 0 && l <= 2) {
+            l = 0;
+        }
+        else if(l >= 3 && l <=5) {
+            l = 3;
+        }
+        else {
+            l = 6;
+            System.out.println("i");
+        }
+
+        // work on the mini grid section
+
+        return rowValues;
+    }
+
+    public static void main(String[] args) {
+        Cell[][] board = new Cell[4][4];
+        board[0][0] = new Cell(0,0,0);
+        board[0][1] = new Cell(4,0,1);
+        board[0][2] = new Cell(1,0,2);
+        board[0][3] = new Cell(0,0,3);
+
+        board[1][0] = new Cell(1,1,0);
+        board[1][1] = new Cell(0,1,1);
+        board[1][2] = new Cell(0,1,2);
+        board[1][3] = new Cell(0,1,3);
+
+        board[2][0] = new Cell(0,2,0);
+        board[2][1] = new Cell(0,2,1);
+        board[2][2] = new Cell(0,2,2);
+        board[2][3] = new Cell(2,2,3);
+
+        board[3][0] = new Cell(0,3,0);
+        board[3][1] = new Cell(2,3,1);
+        board[3][2] = new Cell(3,3,2);
+        board[3][3] = new Cell(0,3,3);
+
+        Sudoku sudoku = new Sudoku();
+        sudoku.solve(board);
+
+
+
+    }
 }
