05e0bd6f18651448a040800de57307b2f2bd1632
Darci Khey Saucedo <darcikhey27@gmail.com>
2017-12-27 16:59:53 -0800

    added site controller

---
+++ src/main/java/com/darcikhey/sudoku/SudokuController.java
@@ -0,0 +1,11 @@
+package com.darcikhey.sudoku;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by DARCI on 12/27/2017.
+ */
+@Controller
+public class SudokuController {
+    
+}
